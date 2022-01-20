827be398da640708a14f7a12456ee616b9af1c03
logiczsniper <lczernel@gmail.com>
2022-01-20 16:23:16 +0000

    feat: progress on ProFeature widget

--- lib/common/widgets/pro_feature.widget.dart
+++ lib/common/widgets/pro_feature.widget.dart
@@ -2,14 +2,22 @@ import 'package:dishful/common/data/providers.dart';
 import 'package:dishful/common/domain/user_meta.dart';
 import 'package:dishful/common/services/auth.service.dart';
 import 'package:dishful/common/services/db.service.dart';
+import 'package:dishful/theme/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flash/flash.dart';
 
 class ProFeature extends ConsumerWidget {
   late final AsyncValueProvider<UserMeta?> userMetaProvider;
+  final String why;
   final Widget child;
 
-  ProFeature({required this.child, Key? key}) : super(key: key) {
+  ProFeature({
+    required this.child,
+    required this.why,
+    Key? key,
+  }) : super(key: key) {
     final id = AuthService.currentUser?.uid;
     if (id == null) throw "No current user";
 
@@ -21,6 +29,49 @@ class ProFeature extends ConsumerWidget {
     final isPro = ref.watch(
       userMetaProvider.select((value) => value.asData?.value?.isPro ?? false),
     );
-    return Container();
+
+    return isPro
+        ? child
+        : GestureDetector(
+            onTap: () => context.showFlashDialog(
+              constraints: BoxConstraints(maxWidth: 400),
+              title: Text("This is a pro feature :("),
+              content: Column(
+                children: [
+                  Text(
+                    "Why?",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(why),
+                ],
+              ),
+              positiveActionBuilder: (context, controller, _) {
+                return TextButton(
+                  onPressed: () {
+                    // TODO: open subscription stuff
+                    controller.dismiss();
+                  },
+                  child: Text("Upgrade"),
+                );
+              },
+              negativeActionBuilder: (context, controller, _) {
+                return TextButton(
+                  onPressed: controller.dismiss,
+                  child: Text("Ok"),
+                );
+              },
+            ),
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              fit: StackFit.expand,
+              children: [
+                Icon(
+                  FontAwesomeIcons.crown,
+                  color: Palette.primaryDark.withAlpha(100),
+                ),
+                child
+              ],
+            ),
+          );
   }
 }
